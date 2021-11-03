5f7d682b9008450e7f1d8c750bce54ba9b3010ca
Chloe Park <jeonghwapark@outlook.com>
2021-11-03 15:02:21 +0900

    20211103_001 - 주소 테스트 추가

--- BuyOrRent/src/main/java/com/jeonghwapark/buyorrent/controller/TradeBoardController.java
+++ BuyOrRent/src/main/java/com/jeonghwapark/buyorrent/controller/TradeBoardController.java
@@ -1,6 +1,13 @@
 package com.jeonghwapark.buyorrent.controller;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -62,5 +70,22 @@ public class TradeBoardController {
 		tSrv.deleteArticleOne(bid);
 		
 		return "success";
+	}
+	
+	// 주소 호출 
+	@ResponseBody
+	@RequestMapping(value="/address", method=RequestMethod.POST)
+	public String getAddressList(HttpServletRequest request, HttpServletResponse response) throws ServletException, UnsupportedEncodingException, MalformedURLException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		String addr = "http://api.vworld.kr/req/data?service=data&request=GetFeature&data=LT_C_ADEMD_INFO&key=";
+		String serviceKey = "93F80BEB-B3E3-32FB-90D3-A200CDFAC951";
+		String param = "&full_nm";
+		
+		URL url = new URL(addr + serviceKey + param);
+		
+		System.out.println();
+		return "";
 	}
 }
